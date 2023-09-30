de50069bc14af6aa96224ce599edf5c8482e7f05
Muhammet Demircelik <muhammetdemircelik@outlook.com>
2023-09-30 14:58:04 +0300

    first commit

---
+++ src/app/pages/todo-add/todo-add.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TodoAdd } from 'src/app/models/todo-add';
+import { TodoService } from 'src/app/services/todo-service';
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styleUrls: ['./todo-add.component.css']
+})
+export class TodoAddComponent implements OnInit{
+
+  todoForm=new FormGroup({
+    content: new FormControl('')
+  })
+
+  constructor(private todoService:TodoService,private router:Router){ }
+
+  ngOnInit(): void {
+      
+  }
+
+  add()
+  {
+    this.todoService.add(this.todoForm.value as TodoAdd).subscribe(x=>{
+      if(x==true)
+      {
+        this.router.navigateByUrl("/todos")
+      }
+
+    })
+  }
+
+}
